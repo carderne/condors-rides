047dc86195c1a93cbc8a447ffadff15a47284aaa
Chris Arderne <chris@rdrn.me>
2025-05-27 11:45:58 +0100

    reverse sort old rides

--- src/app/(authed)/rides/old/page.tsx
+++ src/app/(authed)/rides/old/page.tsx
@@ -3,7 +3,7 @@ import { groupRidesByDate } from "@/components/rides/list";
 import { GenericRidesPage } from "@/components/rides/rides-page";
 import { getMembership } from "@/dal/membership";
 import { db, schema } from "@/db";
-import { and, isNull, lt } from "drizzle-orm";
+import { and, desc, isNull, lt } from "drizzle-orm";
 
 export default async function OldRides() {
   const user = await getMembership();
@@ -11,7 +11,7 @@ export default async function OldRides() {
   const rides = await db.query.ride.findMany({
     where: and(isNull(schema.ride.deletedAt), lt(schema.ride.date, new Date())),
     with: { leader: true, members: true },
-    orderBy: [schema.ride.date, schema.ride.time, schema.ride.slug],
+    orderBy: [desc(schema.ride.date), schema.ride.time, schema.ride.slug],
   });
   const datedRideArray = groupRidesByDate(rides);
   return <GenericRidesPage rides={datedRideArray} user={user} />;
